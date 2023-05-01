0deb1b725e384937d51a3f6151b64dc2aaeebae0
EverittGill <eg.everittgill@gmail.com>
2023-05-01 19:57:38 -0400

    added accounts routes. ready for testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const accountRoutes = require('./accountRoutes');
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const accountRoutes = require('./accountRoutes');
 
-// // router.use('/accounts', accountRoutes);
-// router.use('/users', userRoutes);
+router.use('/accounts', accountRoutes);
+router.use('/users', userRoutes);
 
 
-// module.exports = router;
+module.exports = router;
