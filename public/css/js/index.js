3c320ad15bb34d4b705a0e87f5821fcd41577fc8
Brosa <brunoarosa1997@gmail.com>
2023-05-01 19:01:51 -0400

    began login submit button

--- public/css/js/index.js
+++ public/css/js/index.js
@@ -2,12 +2,12 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector("#name-signup").value.trim();
-  const username = document.querySelector("#usernmae-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
   const response = await fetch("/api/userRoutes", {
     method: "POST",
-    body: JSON.stringify({ name, username, password }),
+    body: JSON.stringify({ name, email, password }),
     headers: { "Content-Type": "application/json" },
   });
 
@@ -17,6 +17,15 @@ const signupFormHandler = async (event) => {
     alert(response.statusText);
   }
 };
+
+const loginFormHandler = async (event) => {
+  event.preventDefault()
+
+  const email = document.querySelector("email-login").value.trim()
+  const password = document.querySelector("password-login").value.trim()
+
+  const response = await fetch("/api/")
+}
 
 document
   .querySelector(".signup-form")
