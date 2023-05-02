ee4f007c8df7153ccb43e14b6b2ffc5ed0296308
Brosa <brunoarosa1997@gmail.com>
2023-05-01 21:48:35 -0400

    pulling updated

--- public/css/js/index.js
+++ public/css/js/index.js
@@ -5,7 +5,7 @@ const signupFormHandler = async (event) => {
   const email = document.querySelector("#email-signup").value.trim();
   const password = document.querySelector("#password-signup").value.trim();
 
-  const response = await fetch("/api/userRoutes", {
+  const response = await fetch("/api/users", {
     method: "POST",
     body: JSON.stringify({ name, email, password }),
     headers: { "Content-Type": "application/json" },
@@ -21,12 +21,24 @@ const signupFormHandler = async (event) => {
 const loginFormHandler = async (event) => {
   event.preventDefault()
 
-  const email = document.querySelector("email-login").value.trim()
-  const password = document.querySelector("password-login").value.trim()
+  const email = document.querySelector("#email-input").value.trim()
+  const password = document.querySelector("#password-input").value.trim()
 
-  const response = await fetch("/api/")
+  const response = await fetch("/api/users/login", {
+    method: 'POST',
+    body: JSON.stringify({email, password}),
+    headers: {'Content-Type': 'application/json'},
+  })
+
+  if (response.ok) {
+    document.location.replace('/')
+  } else {
+    alert(response.statusText)
+  }
 }
 
 document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector('.login')
+  .addEventListener('submit', loginFormHandler);
+
+  
