43cd815de957a1358a63e66f9e2e1538b6114eec
Brosa <brunoarosa1997@gmail.com>
2023-05-04 19:47:20 -0400

    working on account btn handler

--- public/JS/homepage.js
+++ public/JS/homepage.js
@@ -1,22 +1,24 @@
 const ENCRYPTION_KEY = "MySuperSecretKey";
 
 const accountBtnHandler = async (event) => {
-  event.preventDefault()
-  const accountName = document.querySelector('#account-name')
-  const url = document.querySelector('#url')
-  const password = document.querySelector('#password')
-
-  
-}
+  event.preventDefault();
+  const accountName = document.querySelector("#account-name");
+  const url = document.querySelector("#account-url");
+  const password = document.querySelector("#password");
+};
 
 const revealBtnHandler = async (event) => {
   const password = event.target.getAttribute("data-id");
-  const shownPassword = event.target.previousElementSibling
+  const shownPassword = event.target.previousElementSibling;
   const decryptedPassword = CryptoJS.AES.decrypt(password, ENCRYPTION_KEY);
-  shownPassword.textContent = decryptedPassword.toString(CryptoJS.enc.Utf8)
+  shownPassword.textContent = decryptedPassword.toString(CryptoJS.enc.Utf8);
   return;
 };
 
 document
   .querySelector(".account-list")
   .addEventListener("click", revealBtnHandler);
+
+document
+  .querySelector("#add-account")
+  .addEventListener("click", accountBtnHandler);
