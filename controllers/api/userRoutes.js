30fd57bfbad84fbcc64ea7058e4fee579e19069d
Brosa <brunoarosa1997@gmail.com>
2023-05-04 21:33:24 -0400

    fixed login button and homepage

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,48 +18,5 @@ router.post('/', async (req, res) => {
     }
   });
 
-// posts a login request for an existing user.
-  router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-      console.log(userData)
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-      const validPassword = await userData.checkPassword(req.body.password);
-      
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        console.log(req.session.logged_in); // log the value of logged_in
-        res.redirect('/homepage');
-      });
-      
-  
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-// sends a logout request for the user
-  router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
-  
+
   module.exports = router;
--- public/JS/homepage.js
+++ public/JS/homepage.js
@@ -2,12 +2,28 @@ const ENCRYPTION_KEY = "MySuperSecretKey";
 
 const accountBtnHandler = async (event) => {
   event.preventDefault();
-  const accountName = document.querySelector("#account-name");
-  const url = document.querySelector("#account-url");
-  const password = document.querySelector("#password");
+  const accountName = document.querySelector("#account-name").value.trim();
+  const URL = document.querySelector("#account-url").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  if (accountName & password) {
+    const response = await fetch("/api/accounts", {
+      method: "POST",
+      body: JSON.stringify({ accountName, URL, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      onclose()
+      document.location.replace("/");
+    } else {
+      alert("Failed to add account");
+    }
+  }
 };
 
 const revealBtnHandler = async (event) => {
+  console.log("here i am your reveal")
   const password = event.target.getAttribute("data-id");
   const shownPassword = event.target.previousElementSibling;
   const decryptedPassword = CryptoJS.AES.decrypt(password, ENCRYPTION_KEY);
@@ -20,5 +36,5 @@ document
   .addEventListener("click", revealBtnHandler);
 
 document
-  .querySelector("#add-account")
+  .querySelector(".modal-footer")
   .addEventListener("click", accountBtnHandler);
