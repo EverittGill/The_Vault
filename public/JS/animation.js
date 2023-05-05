12ba61c7fa0ae64a781eb225cfde4c992d23e25b
JacobWillis <jacobwillis2012@gmail.com>
2023-05-05 19:01:52 -0400

    'updated Logout feature and working animation'

--- public/JS/animation.js
+++ public/JS/animation.js
@@ -1,7 +1,5 @@
-const video = document.getElementById("animation");
-const playButton = document.getElementById("play-button");
+const video = document.getElementById("vault_animation");
 
-playButton.addEventListener("click", () => {
-  video.play();
-});
-
+setTimeout(function() {
+  window.location.replace("/homepage");
+}, 10000); 
